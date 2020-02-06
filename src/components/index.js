fc116d30f88b42f753b3ac893131e7c85a2ba92e
mohamadsaffari90@gmail.com <mohamadsaffari90@gmail.com>
2020-02-06 12:43:11 +0330

    complete sub-cap badge and start volume badge logic...

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,86 @@
 import videojs from "video.js";
 
-class ControlBadge {
-  
+const Dom = videojs.dom;
+
+class Badge {
 	constructor(player, options) {
 		this.player_ = player;
 		this.options = options;
 	}
+
+	/**
+	 * The initial function to create element's
+	 *
+	 */
+	createBadges() {
+		const options_ = this.options;
+		if (options_.subtitle) {
+			videojs.log("Create TextTrack");
+			this.createTexTrackBadge(options_.subtitle);
+		}
+		if (options_.volume) {
+			videojs.log("Create VolumeBadge");
+			this.createVolumeBadge(options_.volume);
+		}
+		if (options_.quality) {
+		}
+	}
+	/**
+	 * The function create `TextTrack` badge top of sub-caps button or else subtitle plugin
+	 *
+	 * @param {object} options - { position: 'right'|'left', color:'string', backgroundColor:'string' }
+	 */
+	createTexTrackBadge(options) {
+		const player = this.player_;
+		player.on("texttrackchange", e => {
+			this.onTextTrackChange();
+		});
+	}
+	/**
+	 * The function create `Volume` badge top of volume-bar
+	 *
+	 * @param {object} options - { position: 'right'|'left', color:'string', backgroundColor:'string' }
+	 */
+	createVolumeBadge(options) {
+		const player = this.player_;
+		player.on("volumechange", e => {
+			this.onVolumeChange();
+		});
+	}
+
+	/**
+	 *
+	 */
+	onVolumeChange(options) {
+		videojs.log("volume :", parseInt(this.player_.volume() * 100));
+	}
+
+	/**
+	 *
+	 */
+	onTextTrackChange() {
+		const textTracks = this.player_.textTracks();
+		const subTextDoms = [];
+		subTextDoms.push(Dom.$(".vjs-subtitle-setting-button .vjs-control-text"));
+		subTextDoms.push(Dom.$(".vjs-subs-caps-button .vjs-control-text"));
+		if (subTextDoms && subTextDoms.length > 0) {
+			if (textTracks && textTracks.tracks_.length > 0) {
+				textTracks.tracks_.map(textTrack => {
+					if (textTrack.mode && textTrack.mode === "showing") {
+						subTextDoms.map(subText => {
+							if (subText) {
+								if (subText.innerText !== textTrack.language) {
+									subText.innerText = textTrack.language;
+								}
+							}
+						});
+					}
+				});
+			}
+		}
+	}
 }
 
-ControlBadge.prototype.name = "ControlBadge";
+Badge.prototype.name = "ControlBadge";
 
-export default ControlBadge;
+export default Badge;
--- src/plugin.js
+++ src/plugin.js
@@ -1,7 +1,8 @@
-import videojs from 'video.js';
-import { version as VERSION } from '../package.json';
+import videojs from "video.js";
+import { version as VERSION } from "../package.json";
+import Badge from "./components/index";
 
-const Plugin = videojs.getPlugin('plugin');
+const Plugin = videojs.getPlugin("plugin");
 
 // Default options for the plugin.
 const defaults = {};
@@ -12,39 +13,38 @@ const defaults = {};
  * See: https://blog.videojs.com/feature-spotlight-advanced-plugins/
  */
 class ControlsBadge extends Plugin {
+	/**
+	 * Create a ControlsBadge plugin instance.
+	 *
+	 * @param  {Player} player
+	 *         A Video.js Player instance.
+	 *
+	 * @param  {Object} [options]
+	 *         An optional options object.
+	 *
+	 *         While not a core part of the Video.js plugin architecture, a
+	 *         second argument of options is a convenient way to accept inputs
+	 *         from your plugin's caller.
+	 */
+	constructor(player, options) {
+		// the parent class will add player under this.player
+		super(player);
 
-  /**
-   * Create a ControlsBadge plugin instance.
-   *
-   * @param  {Player} player
-   *         A Video.js Player instance.
-   *
-   * @param  {Object} [options]
-   *         An optional options object.
-   *
-   *         While not a core part of the Video.js plugin architecture, a
-   *         second argument of options is a convenient way to accept inputs
-   *         from your plugin's caller.
-   */
-  constructor(player, options) {
-    // the parent class will add player under this.player
-    super(player);
+		this.options = videojs.mergeOptions(defaults, options);
 
-    this.options = videojs.mergeOptions(defaults, options);
-
-    this.player.ready(() => {
-      this.player.addClass('vjs-controls-badge');
-      this.initial();
-    });
-  }
-  /**
-   * Initial `controls-badge` logic
-   * 
-   */
-  initial() {
-    
-  }
-
+		this.player.ready(() => {
+			this.player.addClass("vjs-controls-badge");
+			this.initial();
+		});
+	}
+	/**
+	 * Initial `controls-badge` logic
+	 *
+	 */
+	initial() {
+		const badge = new Badge(this.player, this.options);
+		badge.createBadges();
+	}
 }
 
 // Define default values for the plugin's `state` object here.
@@ -54,6 +54,6 @@ ControlsBadge.defaultState = {};
 ControlsBadge.VERSION = VERSION;
 
 // Register the plugin with video.js.
-videojs.registerPlugin('controlsBadge', ControlsBadge);
+videojs.registerPlugin("controlsBadge", ControlsBadge);
 
 export default ControlsBadge;
